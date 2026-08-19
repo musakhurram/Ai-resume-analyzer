e69727b39be73b1b3fcaabfa66e201e7763060fa
musakhurram <musakhurram248@gmail.com>
2026-08-19 18:19:37 +0500

    register/login/logout

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const app = require("./src/app")
-
-app.listen(300,()=>{
+const connectToDB =require("./src/config/database")
+connectToDB()
+app.listen(3000,()=>{
     console.log("Server is running on port 3000")
 })
