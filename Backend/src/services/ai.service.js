6899e3afa96755be3297c20837c58cc48961f36a
musakhurram <musakhurram248@gmail.com>
2026-08-22 21:42:06 +0500

    AI schema

---
+++ Backend/src/services/ai.service.js
@@ -0,0 +1,17 @@
+﻿const { GoogleGenAI } = require("@google/genai")
+
+const ai = new GoogleGenAI({
+    apiKey: process.env.GOOGLE_GENAI_API_KEY
+})
+
+async function invokeGeminiAi(prompt = "Hello gemini ! explain what is interview") {
+    const response = await ai.models.generateContent({
+        model: "gemini-3.6-flash",
+        contents: prompt,
+    })
+    console.log(response.text)
+    return response.text
+}
+
+module.exports = invokeGeminiAi
+module.exports.invokeGeminiAi = invokeGeminiAi
