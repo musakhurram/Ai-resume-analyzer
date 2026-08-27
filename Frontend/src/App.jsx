35db87291e4162fe14e15ffd3eb537865cd1c2b3
musakhurram <musakhurram248@gmail.com>
2026-08-27 05:47:27 +0500

    Merge Vercel Analytics into main

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from "react-router"
 import {router} from "./app.routes.jsx"
 import { AuthProvider } from "./features/auth/auth.context.jsx"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <AuthProvider>
     <RouterProvider router={router}/>
+    <Analytics />
     </AuthProvider>
   )
 }
