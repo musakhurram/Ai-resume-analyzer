c92bf31ef4f50641803ca568b638d8ae5a0838d1
musakhurram <musakhurram248@gmail.com>
2026-08-27 17:44:53 +0500

    Wire manual ATS editor controller

--- Backend/src/routes/atsAnalyze.routes.js
+++ Backend/src/routes/atsAnalyze.routes.js
@@ -3,12 +3,13 @@ const upload = require("../middlewares/file.middleware");
 const { aiGenerationLimiter } = require("../middlewares/rateLimit.middleware");
 const { authUser } = require("../middlewares/auth.middleware");
 const atsController = require("../controllers/atsAnalyze.controller");
+const { atsManualRevisionController } = require("../controllers/atsManualRevision.controller");
 
 const atsRouter = express.Router();
 
 atsRouter.post("/ats-analyze", authUser, aiGenerationLimiter, upload.single("resume"), atsController.atsAnalyzeController);
 atsRouter.post("/ats-revise", authUser, aiGenerationLimiter, atsController.atsReviseController);
-atsRouter.put("/ats-revision/:id", authUser, atsController.atsManualRevisionController);
+atsRouter.put("/ats-revision/:id", authUser, atsManualRevisionController);
 atsRouter.get("/ats-download/:id", authUser, atsController.atsDownloadController);
 atsRouter.get("/ats-preview/:id", authUser, atsController.atsPreviewController);
 atsRouter.get("/ats-original/:id", authUser, atsController.atsOriginalController);
