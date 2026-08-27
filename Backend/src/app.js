1f834634f22b263dac4422c9246b9470577c7ab5
musakhurram <musakhurram248@gmail.com>
2026-08-27 15:00:37 +0500

    -

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,6 +15,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const helmet = require("helmet");
+const stripe = require("stripe");
 const compression = require("compression");
 const morgan = require("morgan");
 const env = require("./config/env");
